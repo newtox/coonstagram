c0ea9654f994c1ed61dd93b16bfdccd7597f9e67
newtox <futzy261@gmail.com>
2026-09-15 01:15:31 +0200

    Add missing lang/en/ui.php

---
+++ lang/en/ui.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'meta_description' => 'The South Park tablet feed for Coon and Friends. Post, like, and follow your favorite characters.',
+    'logout' => 'Logout',
+    'back_to_feed' => 'Back to feed',
+    'delete' => 'Delete',
+    'follow' => 'Follow',
+    'following' => 'Following',
+    'language' => 'Language',
+    'select_language' => 'Select language',
+    'german' => 'Deutsch',
+    'english' => 'English',
+    'confirm_title' => 'Are you sure?',
+    'cancel' => 'Cancel',
+    'confirm_delete' => 'Delete',
+];
